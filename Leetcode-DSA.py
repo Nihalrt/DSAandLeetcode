59d91dbdbab837743d203181a03f9de1fb13f9e3
Sai Nihal Diddi <sainihaldiddi@uvic.ca>
2025-02-23 23:33:02 -0800

    solved-isbalanced

--- Leetcode-DSA.py
+++ Leetcode-DSA.py
@@ -857,6 +857,35 @@ class Solution:
         return root
 
 
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# Solved is_balanced leetcode Easy
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def check_height(node):
+            if not node:
+                return 0
+
+            left = check_height(node.left)
+            right = check_height(node.right)
+
+            if left==-1:
+                return -1
+            if right==-1:
+                return -1
+
+            if abs(left - right) > 1:
+                return -1
+
+            return max(left, right) + 1
+        return check_height(root) != -1
+
     
     
 
