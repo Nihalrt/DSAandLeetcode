d894dcee17e153e2c62eb1c19d761d5a6f86b382
Sai Nihal Diddi <sainihaldiddi@uvic.ca>
2025-03-27 00:09:28 -0700

    zigzag-traversal

--- Leetcode-DSA.py
+++ Leetcode-DSA.py
@@ -955,6 +955,44 @@ class Solution:
             result.append(level)
 
         return result
+    # ZigZag level order traversal
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+
+        result = []
+        queue = deque([root])
+        left_to_right = True
+
+        while queue:
+            level_size = len(queue)
+            level = []
+
+            for i in range(level_size):
+                node = queue.popleft()
+                level.append(node.val)
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+                
+            if not left_to_right:
+                level.reverse()
+            
+            result.append(level)
+            left_to_right = not left_to_right
+        return result
+    
+root = TreeNode(3)
+root.left = TreeNode(9)
+root.right = TreeNode(20)
+root.right.left = TreeNode(15)
+root.right.right = TreeNode(7)
+
+# Run the test
+sol = Solution()
+print(sol.levelOrder(root))
         
         
 
